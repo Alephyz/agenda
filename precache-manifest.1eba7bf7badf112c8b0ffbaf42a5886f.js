a377581c9f61391dab32f0f16f3a3045480ed0cd
Alejandro Mora <yalephy@gmail.com>
2020-10-21 18:40:06 -0500

    first commit

---
+++ precache-manifest.1eba7bf7badf112c8b0ffbaf42a5886f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a4fabb2d3109c9243a15095d7866ce2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "346daa789e12917c5ae6",
+    "url": "/static/css/main.53d48ec2.chunk.css"
+  },
+  {
+    "revision": "33be1e857da39ee9447b",
+    "url": "/static/js/2.5fb79d38.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.5fb79d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "346daa789e12917c5ae6",
+    "url": "/static/js/main.174ff934.chunk.js"
+  },
+  {
+    "revision": "7f386176260365bfb20a",
+    "url": "/static/js/runtime-main.284de0bb.js"
+  }
+]);
